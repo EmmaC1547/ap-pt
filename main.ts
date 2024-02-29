adee9809800007a1a0a2bbb4f744cdec757bf03d
EmmaC1547 <156236200+EmmaC1547@users.noreply.github.com>
2024-02-29 12:56:59 -0600

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,150 +1,227 @@
 namespace SpriteKind {
     export const diamond = SpriteKind.create()
 }
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-	
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
+    diamondsCollected += 1
+    info.changeScoreBy(1)
+    tiles.setTileAt(tiles.getTileLocation(23, 5), assets.tile`myTile`)
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.diamond, function (sprite, otherSprite) {
-    let diamond: Image = null
-    if (diamond.equals(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . 5 5 5 5 5 5 5 5 . . . . 
-        . . . . 5 5 5 5 5 5 5 5 . . . . 
-        . . . . 5 5 5 5 5 5 5 . . . . . 
-        . . . . 5 5 5 5 5 5 5 . . . . . 
-        . . . . . 5 5 5 5 5 5 . . . . . 
-        . . . . . . 5 5 5 5 . . . . . . 
-        . . . . . . . 5 5 . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `)) {
-        info.changeScoreBy(1)
-    } else if (diamond.equals(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . a a a a a a a a a . . . 
-        . . . . a a a a a a a a a . . . 
-        . . . . a a a a a a a a a . . . 
-        . . . . . a a a a a a a . . . . 
-        . . . . . a a a a a a . . . . . 
-        . . . . . a a a a a a . . . . . 
-        . . . . . . a a a a . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `)) {
-    	
-    } else if (diamond.equals(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . 8 8 8 8 8 8 8 8 . . . . 
-        . . . 8 8 8 8 8 8 8 8 8 8 . . . 
-        . . . 8 8 8 8 8 8 8 8 8 8 . . . 
-        . . . . 8 8 8 8 8 8 8 8 8 . . . 
-        . . . . . 8 8 8 8 8 8 8 . . . . 
-        . . . . . . 8 8 8 8 8 . . . . . 
-        . . . . . . 8 8 8 8 . . . . . . 
-        . . . . . . . . 8 . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `)) {
-    	
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile15`, function (sprite, location) {
+    info.changeLifeBy(-1)
+    tiles.setTileAt(tiles.getTileLocation(33, 18), assets.tile`myTile`)
+    music.play(music.melodyPlayable(music.powerDown), music.PlaybackMode.UntilDone)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile4`, function (sprite, location) {
+    diamondsCollected += 1
+    info.changeScoreBy(1)
+    tiles.setTileAt(tiles.getTileLocation(20, 15), assets.tile`myTile`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile9`, function (sprite, location) {
+    diamondsCollected += 1
+    info.changeScoreBy(1)
+    tiles.setTileAt(tiles.getTileLocation(17, 27), assets.tile`myTile`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile3`, function (sprite, location) {
+    diamondsCollected += 1
+    info.changeScoreBy(1)
+    tiles.setTileAt(tiles.getTileLocation(8, 14), assets.tile`myTile`)
+})
+function addEnemies (enemyList: Image[]) {
+    for (let value of tiles.getTilesByType(assets.tile`myTile12`)) {
+        enemySprite = sprites.create(enemyList._pickRandom(), SpriteKind.Enemy)
+        tiles.placeOnTile(enemySprite, value)
+        enemySprite.setVelocity(75, 0)
+        enemySprite.setBounceOnWall(true)
     }
-})
-function addEnemies (enemySpawn: any[], diamondsCollected: number) {
-    while (diamondsCollected > 2) {
-        if (diamondsCollected == 3) {
-        	
-        } else if (diamondsCollected == 5) {
-        	
-        }
+    if (diamondsCollected == 3) {
+        tiles.setTileAt(tiles.getTileLocation(33, 18), assets.tile`myTile15`)
+        tiles.setTileAt(tiles.getTileLocation(0, 0), assets.tile`myTile18`)
+        tiles.setTileAt(tiles.getTileLocation(4, 30), assets.tile`myTile15`)
+        tiles.setTileAt(tiles.getTileLocation(0, 0), assets.tile`myTile18`)
     }
 }
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
+    diamondsCollected += 1
+    info.changeScoreBy(1)
+    tiles.setTileAt(tiles.getTileLocation(30, 5), assets.tile`myTile`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile5`, function (sprite, location) {
+    diamondsCollected += 1
+    info.changeScoreBy(1)
+    tiles.setTileAt(tiles.getTileLocation(3, 24), assets.tile`myTile`)
+    tiles.placeOnTile(mySprite, tiles.getTileLocation(33, 23))
+})
+info.onScore(3, function () {
+    addEnemies(list)
+})
+info.onScore(6, function () {
+    game.splash("You collected all the diamonds!")
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    pause(500)
+})
+let enemySprite: Sprite = null
+let list: Image[] = []
 let diamondsCollected = 0
+let mySprite: Sprite = null
 tiles.setCurrentTilemap(tilemap`level`)
-let mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
-let list = [img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `]
+game.showLongText("Your goal is to collect all the color diamonds, but their might be some diamonds that can cause you a life. ", DialogLayout.Center)
+info.setLife(3)
 info.setScore(0)
-info.setScore(diamondsCollected)
+mySprite = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+tiles.placeOnTile(mySprite, tiles.getTileLocation(3, 2))
+controller.moveSprite(mySprite, 100, 70)
+diamondsCollected = 0
+tiles.setTileAt(tiles.getTileLocation(23, 5), assets.tile`myTile1`)
+tiles.setTileAt(tiles.getTileLocation(30, 2), assets.tile`myTile2`)
+tiles.setTileAt(tiles.getTileLocation(8, 14), assets.tile`myTile3`)
+tiles.setTileAt(tiles.getTileLocation(20, 15), assets.tile`myTile4`)
+tiles.setTileAt(tiles.getTileLocation(3, 24), assets.tile`myTile5`)
+tiles.setTileAt(tiles.getTileLocation(17, 27), assets.tile`myTile9`)
+scene.cameraFollowSprite(mySprite)
+list = [
+img`
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 5 d f . . 
+    . . . . b 5 5 1 f f 5 d 4 c . . 
+    . . . . b 5 5 d f b d d 4 4 . . 
+    . b b b d 5 5 5 5 5 4 4 4 4 4 b 
+    b d d d b b d 5 5 4 4 4 4 4 b . 
+    b b d 5 5 5 b 5 5 5 5 5 5 b . . 
+    c d c 5 5 5 5 d 5 5 5 5 5 5 b . 
+    c b d c d 5 5 b 5 5 5 5 5 5 b . 
+    . c d d c c b d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . . f f f f f . . . . . . . 
+    . . . f e e e e e f . . . . . . 
+    . . f d d d d e e e f . . . . . 
+    . c d f d d f d e e f f . . . . 
+    . c d f d d f d e e d d f . . . 
+    c d e e d d d d e e b d c . . . 
+    c d d d d c d d e e b d c . f f 
+    c c c c c d d d e e f c . f e f 
+    . f d d d d d e e f f . . f e f 
+    . . f f f f f e e e e f . f e f 
+    . . . . f e e e e e e e f f e f 
+    . . . f e f f e f e e e e f f . 
+    . . . f e f f e f e e e e f . . 
+    . . . f d b f d b f f e f . . . 
+    . . . f d d c d d b b d f . . . 
+    . . . . f f f f f f f f f . . . 
+    `,
+img`
+    . . f f f . . . . . . . . f f f 
+    . f f c c . . . . . . f c b b c 
+    f f c c . . . . . . f c b b c . 
+    f c f c . . . . . . f b c c c . 
+    f f f c c . c c . f c b b c c . 
+    f f c 3 c c 3 c c f b c b b c . 
+    f f b 3 b c 3 b c f b c c b c . 
+    . c b b b b b b c b b c c c . . 
+    . c 1 b b b 1 b b c c c c . . . 
+    c b b b b b b b b b c c . . . . 
+    c b c b b b c b b b b f . . . . 
+    f b 1 f f f 1 b b b b f c . . . 
+    f b b b b b b b b b b f c c . . 
+    . f b b b b b b b b c f . . . . 
+    . . f b b b b b b c f . . . . . 
+    . . . f f f f f f f . . . . . . 
+    `,
+img`
+    . . . . . . . . . . . c c c c c 
+    . . . . . . . . . c c 7 7 7 6 c 
+    . . . . . . . . c c 7 7 7 c c . 
+    . . . . . . . . c 6 7 7 c . . . 
+    . . . . . . . . c 6 6 6 c . . . 
+    . . . . . . . . c 6 6 6 c c . . 
+    . . . c c c c c c c 6 6 6 c c . 
+    . . c 6 7 7 7 7 6 c c 6 6 6 c . 
+    . c 7 7 7 7 7 7 7 7 c 6 6 6 c c 
+    c 6 7 7 7 7 7 7 7 7 6 c 6 6 6 c 
+    c 7 c 6 6 6 6 c 7 7 7 c 6 6 6 c 
+    f 7 c c 6 6 c c 7 7 7 f 6 6 6 c 
+    f 7 6 f 6 6 f 6 7 7 7 f 6 6 6 c 
+    . f 7 7 7 7 7 7 7 7 6 f 6 6 c . 
+    . c 1 c f f 1 c 7 6 f 6 6 c c . 
+    . c c c c c c c c c c c c . . . 
+    `
+]
+let newenemyList = [img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . c c c c . . . 
+    . . . . . . . c c d d d d c . . 
+    . . . . . c c c c c c d d c . . 
+    . . . c c c 4 4 4 4 d c c c c c 
+    . . c 4 4 1 4 4 4 4 4 1 c c 4 f 
+    . c 4 4 4 4 1 4 4 4 4 d 1 f 4 f 
+    f 4 4 4 4 4 1 4 4 4 4 4 1 f 4 f 
+    f 4 4 f 4 4 1 4 c f 4 4 1 4 4 f 
+    f 4 4 4 4 4 1 c 4 f 4 4 1 f f f 
+    . f 4 4 4 4 1 4 4 f 4 4 d f . . 
+    . . f 4 4 1 4 c c 4 4 d f . . . 
+    . . . f d 4 4 4 4 4 4 c f . . . 
+    . . . . f f 4 4 4 4 c d b c . . 
+    . . . . . . f f f f d d d c . . 
+    . . . . . . . . . . c c c . . . 
+    `, img`
+    . . . . . . . . c c c c c . . . 
+    . . . . . . c c 5 5 5 5 5 c . . 
+    . . . . . c 5 5 5 5 5 5 5 5 c . 
+    . . . . c b b b b b b 5 5 5 c . 
+    . . . . c 1 1 b b 1 b b c c . . 
+    . . . c 1 1 1 b b 1 1 1 c . . . 
+    . . . c 1 1 1 1 b 1 1 1 c . c c 
+    . . . c d 1 1 1 b 1 1 1 b b 5 c 
+    . . c c d 1 c 1 b 1 b 1 5 5 5 c 
+    . c c d d 1 1 1 1 1 b 1 b b 5 c 
+    f d d d 1 1 1 1 1 b b 1 f . c c 
+    f f f 1 1 1 1 1 1 b b b f . . . 
+    . . . f f 1 1 1 b b b 5 5 f . . 
+    . . . . . f f f 5 5 5 5 5 f . . 
+    . . . . . . . . f f f f f f . . 
+    . . . . . . . . . . . . . . . . 
+    `, img`
+    . . 4 4 4 . . . . 4 4 4 . . . . 
+    . 4 5 5 5 e . . e 5 5 5 4 . . . 
+    4 5 5 5 5 5 e e 5 5 5 5 5 4 . . 
+    4 5 5 4 4 5 5 5 5 4 4 5 5 4 . . 
+    e 5 4 4 5 5 5 5 5 5 4 4 5 e . . 
+    . e e 5 5 5 5 5 5 5 5 e e . . . 
+    . . e 5 f 5 5 5 5 f 5 e . . . . 
+    . . f 5 5 5 4 4 5 5 5 f . . f f 
+    . . f 4 5 5 f f 5 5 6 f . f 5 f 
+    . . . f 6 6 6 6 6 6 4 4 f 5 5 f 
+    . . . f 4 5 5 5 5 5 5 4 4 5 f . 
+    . . . f 5 5 5 5 5 4 5 5 f f . . 
+    . . . f 5 f f f 5 f f 5 f . . . 
+    . . . f f . . f f . . f f . . . 
+    `]
+if (info.life() == 0) {
+    game.splash("You lost")
+}
+addEnemies(list)
